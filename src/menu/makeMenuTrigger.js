11cc36d3babb748af1e2f584e8c7a12a1b74fb95
Jason Skuby <jskuby@themxgroup.com>
2016-08-12 14:40:49 -0500

    Allow user-specified onPress event for MenuTrigger

--- src/menu/makeMenuTrigger.js
+++ src/menu/makeMenuTrigger.js
@@ -29,7 +29,14 @@ module.exports = (React, ReactNative, { model }) => {
         ));
       }
       return (
-        <TouchableHighlight onPress={() => { requestAnimationFrame(this.onPress); }} {...this.props}>
+        <TouchableHighlight
+          {...this.props}
+          onPress={() => {
+            requestAnimationFrame(this.onPress);
+            if (this.props.onPress)
+              this.props.onPress();
+          }}
+        >
           <View style={this.props.style}>
             { this.props.children }
           </View>
